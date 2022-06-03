615b84b0f84a923a0109f3b8206ad9f304aad9b6
Marc Szanto <11840265+Xemdo@users.noreply.github.com>
2022-06-03 14:40:18 -0700

    Everything

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
 
 function App() {
+  var lastStreamedUTC = moment.utc('28 May 2022 04:03:12');
+  var timeNowUTC = moment.utc();
+  
+  var diffDays = timeNowUTC.diff(lastStreamedUTC, 'days');
+  var diffHours = timeNowUTC.diff(lastStreamedUTC, 'hours') - (diffDays * 24);
+  var diffMinutes = (timeNowUTC.diff(lastStreamedUTC, 'minutes') - (diffDays * 1440)) % 60;
+  var diffSeconds = (timeNowUTC.diff(lastStreamedUTC, 'seconds') - (diffDays * 86400)) % 60;
+  var diffMilliseconds = (timeNowUTC.diff(lastStreamedUTC, 'milliseconds') - (diffDays * 86400000)) % 1000;
+  var msAsString = (diffMilliseconds < 10 ? '0' : '') + (diffMilliseconds < 100 ? '0' : '') + `${diffMilliseconds}`
+  console.log(moment.utc().format() + " ; " + moment.utc().hours());
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Time since NegNeg left us:<br/>
+          {diffDays} days, {diffHours} hours, {diffMinutes} minutes, {diffSeconds}.{msAsString} seconds
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="sadge.png" className='sadge' onClick={ () => gottem() } />
+        <p class="msg"></p>
       </header>
     </div>
   );
+}
+
+function gottem() {
+  document.getElementsByClassName('sadge')[0].src = 'RIPBOZO.gif';
+  document.getElementsByClassName('msg')[0].innerHTML = '<a href="https://twitch.tv/whodat950">TIME TO WATCH WHODAT INSTEAD<br /><img src="OMEGADANCE.GIF"></a>';
 }
 
 export default App;
